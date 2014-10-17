f9eb6f427e6e72799541edfe1f173a52c64a5374
Kelly Morning <kleemorning@gmail.com>
2014-10-17 15:23:25 -0400

    Google maps

---
+++ app/assets/javascripts/map/gmap.js
@@ -0,0 +1,49 @@
+//$(window).load(function() {
+//  loadScript();
+//});
+
+var ready;
+var map;
+
+function initialize() {
+        
+  var mapOptions = {
+          center: new google.maps.LatLng(44.259725, -76.507314),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.NORMAL,
+          panControl: true,
+          scaleControl: false,
+          streetViewControl: true,
+          overviewMapControl: true
+        };
+        // initializing map
+        map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+        
+   // geocoding 
+      var geocoding  = new google.maps.Geocoder();
+      $("#submit_button_geocoding").click(function(){
+        codeAddress(geocoding);
+      });
+      $("#submit_button_reverse").click(function(){
+        codeLatLng(geocoding);
+      });
+      
+   
+}
+
+
+ready = function loadScript() {
+  console.log("map loading ...");
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  //'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBJYFdplGeKUUEmGZ-vL4ydiSZ09Khsa_o&sensor=false&libraries=drawing'
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+    //'&v=3.14'+
+    //'&key=AIzaSyBJYFdplGeKUUEmGZ-vL4ydiSZ09Khsa_o'+
+    '&libraries=drawing'+
+    '&callback=initialize';
+  document.body.appendChild(script);
+}
+
+$(window).load(ready);
+$(document).on('page:load', ready);
